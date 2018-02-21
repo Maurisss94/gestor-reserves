faa7650ee7bfe6ce4cfcaf2492d284ad83063377
Artic studio developers <developers@articstudio.com>
2018-02-21 12:12:59 +0100

    Change content for analyze post form

--- wp-content/themes/gestor/template-parts/content-page.php
+++ wp-content/themes/gestor/template-parts/content-page.php
@@ -8,79 +8,53 @@
 
 $optionsRecollida = get_field('lloc_recollida_tornada', 'option') ? get_field('lloc_recollida_tornada', 'option') : array();
 $numOcupants = get_field('num_ocupants_maxim', 'options') ? get_field('num_ocupants_maxim', 'options') : 6;
-?>
 
-<div class="container">
-    <div class="py-5 text-center">
-        <img class="d-block mx-auto mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-        <h2><?php the_title(); ?></h2>
-        <p class="lead">Reserva la teva furgoneta</p>
-    </div>
+if(!isset( $_POST['nonce_field']) || !wp_verify_nonce( $_POST['nonce_field'], 'post_form_reserva' )) :
+	print 'Sorry, your nonce did not verify.';
+	exit;
+else:
+	$diaIni = filter_input(INPUT_POST, 'datepicker-ini');
+	$diaFI = filter_input(INPUT_POST, 'datepicker-fi');
+	$llocRecollida = filter_input(INPUT_POST, 'opcio-recollida');
+	$llocRetorn = filter_input(INPUT_POST, 'opcio-tornada');
+	$ocupants = filter_input(INPUT_POST, 'ocupants');
+	$animals = filter_input(INPUT_POST, 'animals-check') == 'Yes' ? 1 : 0;
 
-    <div class="row">
-        <form class="col-md-12" action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="post">
+	var_dump($_POST);
 
-            <input type="hidden" name="action" value="reserva_form">
+	$args = array(
+		'post_type' => 'furgoneta',
+		'post_status' => 'publish',
+		'meta_query' => array(
+			'relation'		=> 'AND',
+			array(
+				'key'		=> 'ocupants_reserva',
+				'compare'	=> '>=',
+				'value'		=> $ocupants,
+			),
+			array(
+				'key'		=> 'accepta_animals',
+				'compare'	=> '=',
+				'value'		=> $animals,
+			)
+		),
+		'order' => 'ASC',
+		'fields' => 'ids'
+	);
+	$furgosDisponibes = query_posts($args);
+	var_dump($furgosDisponibes);
 
-            <div class="form-group row">
-                <label for="data-ini" class="col-sm-2 col-form-label">Data inici</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="datepicker-ini" name="datepicker-ini" value="">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="data-fi" class="col-sm-2 col-form-label">Data Fi</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="datepicker-fi" name="datepicker-fi" value="">
-                </div>
-            </div>
+	$id = wp_insert_post(array(
+	        'post_title'=>'test',
+            'post_type'=>'reserva',
+            'post_content'=>'demo text',
 
-            <?php for($i = 0;$i<2;$i++) : ?>
-            <div class="form-group row">
-                <label for="lloc-tornada" class="col-sm-2 col-form-label">
-                    <?php   if($i == 0): echo 'Lloc de recollida';
-                            else: echo 'Lloc de tornada';
-                            endif;
-                    ?>
-                </label>
-                <div class="col-sm-10">
-                    <select class="form-control" name="opcio-<?php if($i == 0): echo 'recollida';
-                    else: echo 'tornada'; endif; ?>">
-	                    <?php
-	                    if ( $optionsRecollida ) :
-		                    foreach ( $optionsRecollida as $opcio ): ?>
-                                <option value="<?php echo $opcio['nom_lloc']; ?>"><?php echo $opcio['nom_lloc'] . ' ('.$opcio['preu_lloc'].'€)'; ?></option>
-		                    <?php endforeach;
-	                    else :?>
-                            <option>No disposem de llocs de recollida</option>
-	                    <?php endif; ?>
-                    </select>
-                </div>
-            </div>
-            <?php endfor; ?>
+    ));
 
-            <div class="form-group row">
-                <label for="lloc-tornada" class="col-sm-2 col-form-label">Numero d'ocupants</label>
-                <div class="col-sm-10">
-                    <select class="form-control" name="ocupants">
-                        <?php for($i =1;$i<=$numOcupants;$i++) : ?>
-                        <option  value="<?php echo $i; ?>"><?php echo $i; ?></option>
-                        <?php endfor; ?>
-                    </select>
-                </div>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="animals-check" value="Yes" id="animals-check">
-                <label class="form-check-label" for="animals-check">
-                    Viatges amb animals?
-                </label>
-            </div>
-            <br>
-            <br>
 
-            <button type="submit" class="btn btn-primary btn-lg">CERCAR</button>
-        </form>
-
-    </div>
+    ?>
+<div>
 
 </div>
+
+<?php endif ?>
