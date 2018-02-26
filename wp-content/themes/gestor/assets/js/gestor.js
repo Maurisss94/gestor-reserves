9dbfe4c505e9a9a33dd5b88b2c15baefc972d23a
Artic studio developers <developers@articstudio.com>
2018-02-26 12:15:49 +0100

    Refactoring js

--- wp-content/themes/gestor/assets/js/gestor.js
+++ wp-content/themes/gestor/assets/js/gestor.js
@@ -1,50 +1,56 @@
 (function ($) {
 
-    $.datepicker.regional['es'] = {
-        closeText: 'Cerrar',
-        prevText: '<Ant',
-        nextText: 'Sig>',
-        currentText: 'Hoy',
-        monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-        monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
-        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-        dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Juv', 'Vie', 'Sáb'],
-        dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
-        weekHeader: 'Sm',
-        dateFormat: 'dd/mm/yy',
-        firstDay: 1,
-        isRTL: false,
-        showMonthAfterYear: false,
-        yearSuffix: ''
-    };
-    $.datepicker.setDefaults($.datepicker.regional['es']);
-
-    $( "#datepicker-ini" ).datepicker({
-        minDate: '+2D',
-    });
-    $( "#datepicker-fi" ).datepicker({
-        minDate: '+2D',
-        defaultDate: +7
+    $(document).ready(function(){
+        initStepBar();
+        initCalendar();
 
     });
-    /**
-     * TODO: Canviar manera d'indicar el valor al datepicker
-     */
-    var someDate = new Date();
-    var dd = someDate.getDate()+2;
-    var dd2 = someDate.getDate()+7;
-    var mm = someDate.getMonth() + 1;
-    var y = someDate.getFullYear();
-
-    var someFormattedDate = dd + '/'+ mm + '/'+ y;
-    var someFormattedDateFi = dd2 + '/'+ mm + '/'+ y;
-
-    $("#datepicker-ini").attr("value", someFormattedDate);
-    $("#datepicker-fi").attr("value", someFormattedDateFi);
 
 
-    initStepBar();
+    function initCalendar() {
 
+        $.datepicker.regional['es'] = {
+            closeText: 'Cerrar',
+            prevText: '<Ant',
+            nextText: 'Sig>',
+            currentText: 'Hoy',
+            monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+            monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+            dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+            dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Juv', 'Vie', 'Sáb'],
+            dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
+            weekHeader: 'Sm',
+            dateFormat: 'dd/mm/yy',
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: ''
+        };
+        $.datepicker.setDefaults($.datepicker.regional['es']);
+
+        $("#datepicker-ini").datepicker({
+            minDate: '+2D',
+        });
+        $("#datepicker-fi").datepicker({
+            minDate: '+2D',
+            defaultDate: +7
+
+        });
+        /**
+         * TODO: Canviar manera d'indicar el valor al datepicker
+         */
+        var someDate = new Date();
+        var dd = someDate.getDate() + 2;
+        var dd2 = someDate.getDate() + 7;
+        var mm = someDate.getMonth() + 1;
+        var y = someDate.getFullYear();
+
+        var someFormattedDate = dd + '/' + mm + '/' + y;
+        var someFormattedDateFi = dd2 + '/' + mm + '/' + y;
+
+        $("#datepicker-ini").attr("value", someFormattedDate);
+        $("#datepicker-fi").attr("value", someFormattedDateFi);
+    }
 
 
     function initStepBar() {
@@ -54,7 +60,7 @@
             $('[href=#'+nextId+']').tab('show');
             return false;
 
-        })
+        });
 
         $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
 
@@ -67,13 +73,8 @@
 
             //e.relatedTarget // previous tab
 
-        })
+        });
 
-        $('.first').click(function(){
-
-            $('#myWizard a:first').tab('show')
-
-        })
     }
 
 })(jQuery);
